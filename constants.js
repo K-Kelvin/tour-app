1f21a3f51077f989c469e2344781fd0696b053bf
Nivlek <kelvinkijanda@gmail.com>
2021-11-14 06:18:48 +0300

    Final draft

---
+++ constants.js
@@ -0,0 +1,7 @@
+export default {
+    main: "rgb(128, 0, 128)",
+    iris: "rgb(93, 63, 211)",
+    lightviolet: "rgb(207, 159, 255)",
+    violet: "rgb(127, 0, 255)",
+    thistle: "rgb(216, 191, 216)",
+}
